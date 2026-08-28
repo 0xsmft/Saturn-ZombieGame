fa265229ab7dd8e26281bd6e2de07ee6ae3d7864
0xsmft <beastboioioi.dev@gmail.com>
2026-08-28 16:28:03 +0100

    Fixes

--- Source/Kismet/MainMenuUI.cpp
+++ Source/Kismet/MainMenuUI.cpp
@@ -20,24 +20,17 @@ void MainMenuUI::OnInit()
 
 void MainMenuUI::OnDraw( Timestep ts )
 {
-//	g_AluraCanvas->AlignNextItemCenterXY( g_AluraCanvas->CalcTextSize( "Saturn-ZombieGame" ) );
 	g_AluraCanvas->PushFontSize( 32.0f );
 	g_AluraCanvas->AddText( "Saturn-ZombieGame" );
-
-//	g_AluraCanvas->NudgeNextItemPosition( glm::vec2{ 0.0f, 25.0f }, true );
 
 	if( g_AluraCanvas->AddButton( "Play" ) )
 	{
 		g_ActiveScene->TravelToScene( "PlayingField" );
 	}
 
-//	g_AluraCanvas->NudgeNextItemPosition( glm::vec2{ 0.0f, 25.0f }, true );
-
 	if( g_AluraCanvas->AddButton( "Settings" ) )
 	{
 	}
-
-// 	g_AluraCanvas->NudgeNextItemPosition( glm::vec2{ 0.0f, 25.0f }, true );
 
 	if( g_AluraCanvas->AddButton( "Quit" ) )
 	{
